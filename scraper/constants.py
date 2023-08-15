3250e16df3b233f2a9a642d3cf6c02acfc20e9d5
maanvithag <mgongall@iu.edu>
2023-08-15 17:50:11 -0400

    removing scraper code

--- scraper/constants.py
+++
@@ -1,8 +0,0 @@
-# define constants
-
-# get articles links
-ALL_ARTICLES_PAGE_URL = 'https://plato.stanford.edu/published.html'
-SCRAPED_LINKS_FILE_PATH = 'sep_articles_links.json'
-
-# article scraper
-SCRAPED_ARTICLES_FOLDER_PATH = 'sep_articles/'
